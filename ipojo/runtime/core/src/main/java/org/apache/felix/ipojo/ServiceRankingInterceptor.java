396838afd185dab534196699f9575226cc40f7e4
Pierre Bourret <Pierre.Bourret@imag.fr>
2013-05-16 13:43:55 +0200

    service-interceptor: Added ServiceRankingInterceptor interface

---
+++ ipojo/runtime/core/src/main/java/org/apache/felix/ipojo/ServiceRankingInterceptor.java
@@ -0,0 +1,81 @@
+package org.apache.felix.ipojo;
+
+import org.apache.felix.ipojo.util.DependencyModel;
+import org.osgi.framework.ServiceReference;
+
+import java.util.List;
+
+/**
+ * TODO doc
+ *
+ * @param <S>
+ */
+public interface ServiceRankingInterceptor<S> {
+
+    /**
+     * A sorted set service references.
+     *
+     * <p>
+     *     Does <em>not</em> allow {@code null} elements.
+     * </p>
+     *
+     * @param <S>
+     */
+    interface ServiceReferenceSet<S> extends List<ServiceReference<S>> {
+
+        /**
+         * Copies this service reference set.
+         *
+         * @return an independent copy of this service reference set.
+         */
+        ServiceReferenceSet<S> copy();
+
+    }
+
+    /**
+     * Opens the service ranking interception for the given dependency.
+     *
+     * @param dep the dependency that starts to use this interceptor
+     * @param matching the matching set of services for the given dependency
+     * @return a reordered subset of {@code matchingRefs}
+     */
+    ServiceReferenceSet<S> open(DependencyModel dep, ServiceReferenceSet<S> matching);
+
+    /**
+     * Manages the arrival of a service.
+     *
+     * @param dep the dependency that see the service arrival
+     * @param matching the matching set of services for the given dependency
+     * @param service the arriving service
+     * @return a reordered subset of {@code matchingRefs} <em>plus</em> {@code service}
+     */
+    List<ServiceReference<S>> onServiceArrival(DependencyModel dep, ServiceReferenceSet<S> matching, ServiceReference<S> service);
+
+    /**
+     * Manages the modification of a service.
+     *
+     * @param dep the dependency that see the service modification
+     * @param matching the matching set of services for the given dependency
+     * @param service the modified service
+     * @return a reordered subset of {@code matchingRefs}
+     */
+    List<ServiceReference<S>> onServiceModification(DependencyModel dep, ServiceReferenceSet<S> matching, ServiceReference<S> service);
+
+    /**
+     * Manages the departure of a service.
+     *
+     * @param dep the dependency that see the service departure
+     * @param matching the matching set of services for the given dependency
+     * @param service the leaving service
+     * @return a reordered subset of {@code matchingRefs} <em>minus</em> {@code service}
+     */
+    List<ServiceReference<S>> onServiceDeparture(DependencyModel dep, ServiceReferenceSet<S> matching, ServiceReference<S> service);
+
+    /**
+     * Closes the service ranking interception for the given dependency.
+     *
+     * @param dep the dependency that stops to use this interceptor.
+     */
+    void close(DependencyModel dep);
+
+}
